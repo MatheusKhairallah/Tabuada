9eec04048fcb3d38098b4f5dc6599848048598bf
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2020-07-03 21:24:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro positivo: ");
+            int numero=Convert.ToInt16(Console.ReadLine());
+
+            Console.Clear();
+            
+            for (int i = 0; i <= 10; i=i+1)
+            {
+               Console.WriteLine(numero + " X " +i+ " = " + (numero*i)); 
+            }
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+            
+               
+            
+        }
+    }
+}
